148991a27670d26a2eb29f0964078b4d656bbcec
Richard Lincoln <r.w.lincoln@gmail.com>
2015-05-31 23:39:05 +0200

    Create __init__.py

---
+++ pydyn/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2014-2015 Julius Susanto. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+"""
+PYPOWER-Dynamics
+Time-domain simulation engine
+
+"""
